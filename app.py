2aa908a29f7deaa8123c974e0e5986cd0e5f4569
Tyson Tran <tysontran@Tysons-MBP.hitronhub.home>
2023-06-20 16:57:24 -0700

    Figuring out step 6, having troubles with saving highscore to session

--- app.py
+++ app.py
@@ -1,36 +1,56 @@
+import time
+
 from boggle import Boggle
-from flask import Flask, request, render_template, redirect, flash, session, jsonify
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_debugtoolbar import DebugToolbarExtension
-
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 app.debug = True
+
 #This is a instance 
 boggle_game = Boggle()
 
 @app.route('/')
 def show_boggle():
+    #Create the game board
     board = boggle_game.make_board()
+    #Store board to session
     session['board'] = board
-    print('hi')
-    return render_template('index.html', board=board)
+
+    return render_template('index.html', board=board, time=time.time())
 
 @app.route('/guess')
 def guess():
+    #Grab word from get request
     word = request.args.get('guess')
+    #Grab board from session
     board = session['board']
-    response = boggle_game.check_valid_word(board, word)
+    #Function checks if word is valid/On the game board
+    is_valid = boggle_game.check_valid_word(board, word)
 
-    return jsonify({'result': response})
+    #Return "ok"/"not-on-board"/"not-word" as JSON to client side
+    return jsonify({"result": is_valid})
 
+@app.route('/game-over', methods = ['POST'])
+def gameOver():
 
+#Grab the score sent from Client-Side
+    score = request.json['score']
 
+#Grab the highscore/number of amount times played from session, if there is not any in the session, set them to 0
+    highscore = session.get('highscore', 0)
+    played_times = session.get('played_times', 0)
 
+#If the score that was sent in was higher than the high score, set the new highscore.
+    if int(score) > highscore:
+        session['highscore'] = int(score)
+#Increment the amount of times played
+    session['played_times'] = played_times + 1
 
-
+#Return the new amount of times played/Send back updated highscool/Send back score
+    return jsonify({"score": score, "highscore": session['highscore'], "played": played_times})
 
 
--- static/app.js
+++ static/app.js
@@ -1,13 +1,71 @@
-let form = document.querySelector("form");
-async function getGuess() {
-  let guess = document.querySelector("#guess").value;
-  let res = await axios.get("/guess", { params: { guess: guess } });
+//Grab the form
+let form = document.getElementById("form");
+//Grab the total score
+let total = document.getElementById("score");
+//Grab the high score
+let highscore = document.getElementById("highscore");
+//Grab the number of amount times played
+let playedTimes = document.getElementById("times-played");
+
+//Initialize score of 0 at the beginning of the game
+let score = 0;
+//Update the interface to represent the score
+total.innerHTML = score;
+
+async function handleSubmit(e) {
+  //Prevents refresh of page
+  e.preventDefault();
+  //Grab Guess from the input value
+  let guess = document.getElementById("guess").value;
+  console.log("Guess:", guess);
+  //Request a response to our FLASK server
+  let response = await axios.get("/guess", { params: { guess: guess } });
+  console.log("Response:", response);
+  //Store that response
+  result = response.data.result;
+  console.log("Result:", result);
+  //If the word is valid, tell the user
+  if (result === "ok") {
+    alert("Valid word!");
+    score += guess.length;
+    total.innerHTML = "Score: " + score;
+    console.log("Score: ", score);
+  }
+  //If the word is not-on-board, tell the user
+  else if (result === "not-on-board") {
+    alert("Word is not on the board");
+  }
+  //If the word is not-word, tell the user
+  else if (result === "not-word") {
+    alert("Not a valid word");
+  }
+  //Reset the input value when you submit/guess a word
+  guess = "";
 }
 
-function handleResponse(data) {
-  console.log("Server response:", data);
+async function gameOver() {
+  //Change the innerHTML of score to a actually integer instead of string
+  let scoreText = total.innerHTML.replace("Score ", "");
+  let score = parseInt(scoreText);
+
+  //Send to server
+  let response = await axios.post("/game-over", { score: score });
+
+  //Display Highscore/Amount of times played
+  highscore.innerHTML = "Highscore: " + response.data.highscore;
+  playedTimes.innerHTML = "You have played " + response.data.played + " times";
+
+  console.log(response);
 }
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  getGuess();
-});
+
+//60 Second Timer
+function handleTimer() {
+  alert("Time is up! Game over");
+  guess.disabled = true;
+  gameOver();
+}
+
+let timer = setTimeout(handleTimer, 60000);
+
+//Submit the word/form
+form.addEventListener("submit", handleSubmit);
